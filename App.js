37757c705a3fe125da2b82ea896cff8597dfda26
Rodrigo Zandonadi <rodzandonadi@gmail.com>
2022-05-30 19:50:07 -0300

    Update .gitignore, App.js, app.json, yarn.lock

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>My First App in React Native from Alura Courses</Text>
       <StatusBar style="auto" />
     </View>
   );
