4879774de61c5f5d637cece90266202a2581fdfd
Rodrigo Zandonadi <rodzandonadi@gmail.com>
2022-05-30 20:39:30 -0300

    Import images from project in Cest component

--- App.js
+++ App.js
@@ -1,24 +1,14 @@
 import React from 'react'
 
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar, SafeAreaView } from 'react-native';
 
 import Cesta from './src/telas/Cesta'
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <SafeAreaView >
+      <StatusBar />
       <Cesta />
-      <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
