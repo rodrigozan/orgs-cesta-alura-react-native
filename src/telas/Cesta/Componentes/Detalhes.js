ed53f29c8fdb3a000edadf4182369dfa9b0aba72
Rodrigo Zandonadi <rodzandonadi@gmail.com>
2022-05-31 16:45:00 -0300

    Created a button in Detalhes component

--- src/telas/Cesta/Componentes/Detalhes.js
+++ src/telas/Cesta/Componentes/Detalhes.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import { StyleSheet, Image, View } from 'react-native'
+import { StyleSheet, Image, View, TouchableOpacity } from 'react-native'
 
 // Components
 import Texto from '../../../componentes/Texto'
 
-export default function Detalhes({ nome, logoFazenda, nomeFazenda, descricao, preco}) {
+export default function Detalhes({ nome, logoFazenda, nomeFazenda, descricao, preco, botao }) {
     return <>
         <Texto style={ estilos.nome }>{ nome }</Texto>
         <View style={ estilos.fazenda }>
@@ -14,6 +14,10 @@ export default function Detalhes({ nome, logoFazenda, nomeFazenda, descricao, pr
 
         <Texto style={ estilos.descricao }>{ descricao }</Texto>
         <Texto style={ estilos.preco }> { preco }</Texto>
+
+        <TouchableOpacity style={ estilos.botao } onPress={() => {}}>
+            <Texto style={ estilos.textoBotao }>{ botao }</Texto>
+        </TouchableOpacity>
     </>
 }
 
@@ -49,5 +53,18 @@ const estilos = StyleSheet.create({
         fontSize: 26,
         lineHeight: 42,
         marginTop: 8
+    },
+    botao: {
+        marginTop: 16,
+        backgroundColor: "#2a9f85",
+        paddingVertical: 16,
+        borderRadius: 6
+    },
+    textoBotao: {
+        textAlign: "center",
+        color: "#FFFFFF",
+        fontSize: 16,
+        lineHeight:26,
+        fontWeight: "bold"
     }
 })
