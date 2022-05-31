fd07f7c552d6137f2cf8dde6540a0c4ad9a87a62
Rodrigo Zandonadi <rodzandonadi@gmail.com>
2022-05-31 16:33:48 -0300

    Importing Apploading from Expo

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 import { StatusBar, SafeAreaView, View } from 'react-native';
 import { useFonts, Montserrat_400Regular, Montserrat_700Bold } from '@expo-google-fonts/montserrat';
+import AppLoading from 'expo-app-loading';
 
 import Cesta from './src/telas/Cesta'
 import mock from './src/mocks/cesta'
@@ -13,7 +14,7 @@ export default function App() {
   });
 
   if(!fonteCarregada) {
-    return <View />
+    return <AppLoading />
   }
 
 
