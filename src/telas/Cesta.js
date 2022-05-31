6c8b546bb68e454352bb84c9f1bcf95114de6d68
Rodrigo Zandonadi <rodzandonadi@gmail.com>
2022-05-31 11:08:27 -0300

    Refactory Cesta screen, created Texto component

--- src/telas/Cesta.js
+++ src/telas/Cesta.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Text, View, Image, StyleSheet, Dimensions } from 'react-native'
 
+import Texto from '../componentes/Texto'
+
 import topo from '../../assets/topo.png'
 import logo from '../../assets/logo.png'
 
@@ -9,16 +11,16 @@ const width = Dimensions.get('screen').width
 export default function Cesta() {
     return <>
         <Image source={topo} style={estilos.topo} />
-        <Text style={estilos.titulo}>Detalhe da Cesta</Text>
+        <Texto style={estilos.titulo}>Detalhe da Cesta</Texto>
         <View style={estilos.cesta}>
-            <Text style={estilos.nome}>Cesta de Verduras</Text>
+            <Texto style={estilos.nome}>Cesta de Verduras</Texto>
             <View style={estilos.fazenda}>
                 <Image source={logo} style={estilos.imgFazenda} />
-                <Text style={estilos.nomeFazenda}>Jenny Jack Farm</Text>
+                <Texto style={estilos.nomeFazenda}>Jenny Jack Farm</Texto>
             </View>
 
-            <Text style={estilos.descricao}>Uma cesta com produtos selecionados cuidadosamente da fazenda direto para sua cozinha</Text>
-            <Text style={estilos.preco}>R$ 40,00</Text>
+            <Texto style={estilos.descricao}>Uma cesta com produtos selecionados cuidadosamente da fazenda direto para sua cozinha</Texto>
+            <Texto style={estilos.preco}>R$ 40,00</Texto>
         </View>
     </>
 }
@@ -46,7 +48,7 @@ const estilos = StyleSheet.create({
         color: "#464646",
         fontSize: 26,
         lineHeight: 42,
-        fontFamily: "MontserratBold"
+        fontWeight: "bold"
     },
     fazenda: {
         flexDirection: "row",
@@ -61,7 +63,6 @@ const estilos = StyleSheet.create({
         fontSize: 16,
         lineHeight: 26,
         marginLeft: 12,
-        fontFamily: "MontserratRegular"
     },
     descricao: {
         color: "#a3a3a3",
