e90bbcf3b2d1ebff9228dd8dc383ebc530e746d5
Rodrigo Zandonadi <rodzandonadi@gmail.com>
2022-05-31 17:21:36 -0300

    Using map of JS for list itens

--- src/telas/Cesta/index.js
+++ src/telas/Cesta/index.js
@@ -1,17 +1,19 @@
 import React from 'react'
-import { View, Image, StyleSheet } from 'react-native'
+import { View, Image, StyleSheet, ScrollView } from 'react-native'
 
 // Components
 import Topo from './Componentes/Topo'
 import Detalhes from './Componentes/Detalhes'
+import Itens from './Componentes/Itens'
 
-export default function Cesta({ topo, detalhes, }) {
-    return <>
+export default function Cesta({ topo, detalhes, itens }) {
+    return <ScrollView>
         <Topo { ...topo } />
         <View style={estilos.cesta}>
             <Detalhes { ...detalhes } />
+            <Itens { ...itens }/>
         </View>
-    </>
+    </ScrollView>
 }
 
 const estilos = StyleSheet.create({
